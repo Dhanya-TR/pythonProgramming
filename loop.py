36520fc8dd83f2ad829960bfd3904ff8f26a4b2a
LENOVO <dhanyaravindra4@gmail.com>
2022-11-06 17:11:45 +0530

    tutorial

---
+++ loop.py
@@ -0,0 +1,82 @@
+# #finding the sum of first n whole numbers using for loop
+# endpoint=int(input("enter the endpoint"))
+# sum_of_number=0
+# for i in range(endpoint):
+#     print("sum of number value is:",sum_of_number)
+#     print("index i value is:",i)
+#     sum_of_number=sum_of_number+i
+# print("the sum of first between {} whole number is{}".format(endpoint,sum_of_number))
+#finding the odd number betweem the two points
+# startpoint=int(input("enter the startpoint"))
+# endpoint=int(input("enter the emdpoint"))
+# step=int(input("enter the step"))
+# sumofnumber=0
+# for i in range(startpoint,endpoint,step):
+#     print(i)
+#     sumofnumber=sumofnumber+i
+# print("the sum of odd number between {} and {} is{}".format(startpoint,endpoint,sumofnumber))
+#using the loop for else
+# startpoint=int(input("enter the start point)
+#endpoint=int(input("enter the endpoint)
+# for i in range(startpoint,endpoint):
+#     print(i)
+# else:
+#     print("the loop is ended")
+# endpoint=int(input("enter the endpoint"))
+# sumofnumbers=0
+# i=0
+# while i<=endpoint:
+#     sumofnumbers=sumofnumbers+i
+#     i=i+1
+#     print(i)
+# print("the sum of first {} wholenumber is {}".format(endpoint,sumofnumbers))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
