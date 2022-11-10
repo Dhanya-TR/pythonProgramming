72f233ad11f38748b9330ca41d7f4cd7abc12b80
Dhanya-TR <117572429+Dhanya-TR@users.noreply.github.com>
2022-11-10 19:25:43 +0530

    Add files via upload

--- chatbot.py
+++ chatbot.py
@@ -1,21 +1,16 @@
 print("hello,I am a chatbot")
 print("how may i help you\n")
-print("hit 1 for cricket")
-print("hit 2 for customer care")
-print("hit 3 for news")
-print("hit 4 for daily horoscope")
-print("hit 5 for english novel")
+print("hit 1 to apply voter ID")
+print("hit 2 for modify voter ID")
 #accepting our request
 userinput=int(input("enter your choice:"))
 #using nested if to process the user input
 if userinput==1:
-   inputneeded=input("provide country name")
-if userinput==2:
-    inputneeded=input("provide your query")
-if userinput==3:
-    inputneeded=input("provide the your interest")
-if userinput==4:
-    inputneeded=input("provide your zodiac")
-if userinput==5:
-    inputneeded = input("provide your genre")
-print("thank you for using  our service,our team will get back to you soon")
+    inputneeded=int(input("enter the age of the candidate"))
+    if (inputneeded>=18):
+        print("enter your details")
+    else:
+        print("not eligible for voting")
+else:
+        print(" provide details for modification")
+print("thank you for accepting our service ,we will get back to you soon")
