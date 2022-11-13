af893597199bc3c2422d544be96a4c22ef9eaddc
Dhanya_TR <117572429+Dhanya-TR@users.noreply.github.com>
2022-11-13 18:35:49 +0530

    Add files via upload

--- hidden message.py
+++ hidden message.py
@@ -1,13 +1,18 @@
+# #sending a hidden message
+# encrypt="dhanya"
+# #reverse the string
+# num=encrypt[::-1]
+# vow="aeiou"
+# x=''
+# #replace the vowels using loops
+# for i in num:
+#     if(i in vow):
+#         x+=str(vow.index(i))
+#     else:
+#         x+=i
+# print(x+'aca')
 #sending a hidden message
-encrypt="dhanya"
-#reverse the string
-num=encrypt[::-1]
-vow="aeiou"
-x=''
-#replace the vowels using loops
-for i in num:
-    if(i in vow):
-        x+=str(vow.index(i))
-    else:
-        x+=i
-print(x+'aca')
+value=(input("enter a message"))
+for i in value :
+ print(chr(ord(i)+1))
+
