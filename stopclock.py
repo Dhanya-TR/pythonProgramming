0529f2b6580289fc1a956fce0ab4edb78a9c25a4
Dhanya_TR <117572429+Dhanya-TR@users.noreply.github.com>
2022-11-18 18:53:13 +0530

    Add files via upload

--- stopclock.py
+++ stopclock.py
@@ -1,8 +1,31 @@
+# import time
+#  n=int(input("enter the seconds"))
+#  for i in range(n,0,-1):
+#      print(i)
+#      time.sleep(1)
+
 import time
-n=int(input("enter the seconds"))
-for i in range(n,0,-1):
-    print(i)
-    time.sleep(1)
-print("Time Up")
 
+def convert_count(timer):
+    minute=0
+    seconds=0
+    while timer >= 60:
+        minute += 1
+        timer -= 60
+    seconds = timer
+    print(minute , ":" , seconds)
+    while minute > 0:
+        minute = count(minute,seconds)
+        seconds = 59
+    for i in range(seconds ,0,-1):
+        print(minute,":",i)
+        time.sleep(1)
+    print("Time up!")
 
+def count(minute,seconds):
+    for i in range(seconds,0,-1):
+        print(minute,":",i)
+        time.sleep(1)
+    return minute-1
+timer=int(input("enter the time in seconds:"))
+convert_count(timer)
